704c963d29eec59084b6381f7808139ce4ff1786
Petar Shomov <petar@spretturmarimo.is>
2011-04-25 09:28:48 +0000

    web server config at the same place as the old script

--- WebServer/fabfile.py
+++ WebServer/fabfile.py
@@ -12,13 +12,14 @@ from fabric.operations import sudo
 import fabutils
 import tempfile
 
+mono_version = "2.10.1"
+mono_xsp_version = "2.10"
+libgdi_version = "2.10"
+
 def accounts():
     fabutils._create_account(user = "web", passwd = "web", public_key="web_id.pub")
     
 def install_dotnet_xsp():
-    mono_version = "2.10.1"
-    mono_xsp_version = "2.10"
-    libgdi_version = "2.10"
     if not exists("/opt/mono-%s" % mono_version):
         fabutils.install_mono(mono_version)
     build_mono_xsp(mono_version, mono_xsp_version)
@@ -31,6 +32,10 @@ def install_nginx():
     sudo("update-rc.d nginx enable")
     put("default","/etc/nginx/sites-enabled/default", use_sudo=True)
     sudo("/etc/init.d/nginx restart")
+    with fabutils.process_erb("fastcgi-mono-server4.sh.erb", {"MONO_VERSION" : mono_version}) as f:
+        put(f.name, "/home/web/fastcgi-mono-server4.sh", use_sudo=True)
+        sudo("chmod +x /home/web/fastcgi-mono-server4.sh")
+    
         
 def build_mono_xsp(mono_version, mono_xsp_version):
     sudo("apt-get -y install build-essential autoconf automake pkg-config")
