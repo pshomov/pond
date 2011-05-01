311f00422b5d3db1de418fa1265ec5078cf2cec2
Petar Shomov <petar@spretturmarimo.is>
2011-05-01 16:22:06 +0000

    adding top level script for configuring of an agent server

--- AgentServer/fabfile.py
+++ AgentServer/fabfile.py
@@ -1,11 +1,10 @@
 import sys
 import os
 
-base_folder = os.path.normpath(os.path.abspath(os.path.dirname(__file__))+'/..')
-sys.path[0:0] = [base_folder]
+base_folder = os.path.normpath(os.path.abspath(os.path.dirname(__file__)))
+sys.path[0:0] = [base_folder+"/.."]
 
 from fabric.api import *
-from fabric.contrib.console import confirm
 from fabric.contrib.files import contains,append
 from fabenv import env
 from fabric.operations import sudo
@@ -15,18 +14,18 @@ mono_version = "2.10.1"
 libgdi_version = "2.10"
 
 def accounts():
-    fabutils._create_account(user = "agent", passwd = "agent", public_key="agent_id.pub")
+    fabutils._create_account(user = "agent", passwd = "agent", public_key=base_folder+"/agent_id.pub")
 
 def setup():
     sudo("apt-get -y install git-core curl ruby-full p7zip-full")
-    with fabutils.process_erb("runz-agent.sh.erb", {"MONO_VERSION" : mono_version}) as f:
+    with fabutils.process_erb(base_folder+"/runz-agent.sh.erb", {"MONO_VERSION" : mono_version}) as f:
         put(f.name, "/home/agent/runz-agent.sh", use_sudo=True)
         sudo("chmod +x /home/agent/runz-agent.sh")
     if not contains("/etc/environment", "RUNZ_RABBITMQ_SERVER"):
         append("/etc/environment", "RUNZ_RABBITMQ_SERVER=%s" % env.roledefs['queue'], use_sudo=True)
         
 def install_ruby_support():
-    put("bins/rubygems-1.7.2.tgz")
+    put(base_folder+"/bins/rubygems-1.7.2.tgz")
     run("tar -xf rubygems-1.7.2.tgz")
     with cd("rubygems-1.7.2"):
         sudo("ruby setup.rb")
@@ -36,8 +35,8 @@ def install_ruby_support():
     fabutils._install("libxml2 libxml2-dev")
     fabutils._install("libxslt-ruby1.8 libxslt1-dev")
     fabutils._install("rake")
-    sudo("gem1.8 install bundler rack")
-    sudo("gem1.8 install therubyracer")
+    sudo("gem1.8 install bundler rack --no-rdoc --no-ri")
+    sudo("gem1.8 install therubyracer --no-rdoc --no-ri")
     
 def install_dotnet():
     fabutils.install_mono(mono_version)
@@ -46,5 +45,5 @@ def install_dotnet():
 def install_nunit(version):
     mono_runz_addons = "/opt/mono-%s/runz" % version
     sudo("mkdir -p %s/bin" % mono_runz_addons)
-    put("bins/NUnit", mono_runz_addons, mirror_local_mode=True, use_sudo=True)
-    put("nunit", "%s/bin" % mono_runz_addons, mirror_local_mode=True, use_sudo=True)
+    put(base_folder+"/bins/NUnit", mono_runz_addons, mirror_local_mode=True, use_sudo=True)
+    put(base_folder+"/nunit", "%s/bin" % mono_runz_addons, mirror_local_mode=True, use_sudo=True)
---
+++ fabfile.py
@@ -0,0 +1,14 @@
+from fabric.decorators import roles
+from fabric.operations import sudo
+import AgentServer.fabfile
+
+@roles('agents')
+def agent_server():
+    _apt_update()
+    AgentServer.fabfile.accounts()
+    AgentServer.fabfile.setup()
+    AgentServer.fabfile.install_ruby_support()
+    AgentServer.fabfile.install_dotnet()
+
+def _apt_update():
+    sudo("apt-get -y update")
