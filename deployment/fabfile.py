1647350e27d572e2199829dc2d3eebb84823410a
Petar Shomov <petar@spretturmarimo.is>
2011-05-18 01:15:42 +0000

    the pond for the webz is barely working, needs some love

--- deployment/fabfile.py
+++ deployment/fabfile.py
@@ -16,18 +16,18 @@ def deploy_web(build_output):
     WEB_ARCHIVE = "web.7z"
     if os.path.exists(WEB_ARCHIVE):
         os.remove(WEB_ARCHIVE)
-    local("7z a -r %s %s/web" % (WEB_ARCHIVE, build_output))
-    if exists("runz/web"):
+    local("7z a -r %s %s/webz" % (WEB_ARCHIVE, build_output))
+    if exists("runz/webz"):
         run("rm -rdf runz")
         
-    run("mkdir -p runz/web")
+    run("mkdir -p runz/webz")
     put(WEB_ARCHIVE, "runz/")
-    run("~/fastcgi-mono-server4.sh stop")
+    run("~/runz-webz.sh stop")
     
     run("7z x -orunz runz/%s" % WEB_ARCHIVE)
-    run("chmod -R 755 runz/web")
+    run("chmod -R 755 runz/webz")
 
-    fabutils._run_background_process("~/fastcgi-mono-server4.sh start", "web")
+    run("~/runz-webz.sh start")
 
 def deploy_agent(build_output):
     AGENT_ARCHIVE = "agent.7z"
