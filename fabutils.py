a3fe993c95919317d901e937f648149176656686
Petar Shomov <petar@spretturmarimo.is>
2011-05-27 09:22:32 +0000

    apparently __page__ exists only for packages, not for modules + cleanup

--- fabutils.py
+++ fabutils.py
@@ -1,13 +1,12 @@
 import os
 from fabric.api import *
-from fabric.contrib.console import confirm
-from fabric.contrib.files import contains,append,exists
+from fabric.contrib.files import contains, append, exists
 from fabenv import env
 from fabric.operations import sudo
 import tempfile
-import sys
 
-agent_folder = os.path.normpath(os.path.abspath(os.path.dirname(__file__))+'/AgentServer')
+base = os.path.abspath(os.path.dirname(__file__))
+agent_folder = os.path.join(base, 'AgentServer')
 
 def _create_account(user, passwd, public_key=None):
     sudo("apt-get -y install makepasswd")
@@ -15,14 +14,16 @@ def _create_account(user, passwd, public_key=None):
     env.warn_only = True
     sudo("userdel -f -r %s" % user)
     env.warn_only = False
-    sudo("useradd -d /home/%(user)s -m %(user)s -p '%(passwd)s' -r -G ssh,sudo -s /bin/bash" % {"user" : user, "passwd" : passwd})
+    sudo("useradd -d /home/%(user)s -m %(user)s -p '%(passwd)s' -r -G ssh,sudo -s /bin/bash" % {"user": user,
+                                                                                                "passwd": passwd})
     if public_key is not None:
         sudo("mkdir -p /home/%s/.ssh" % user)
         put(public_key, "/home/%s/.ssh/authorized_keys" % user, use_sudo=True)
 
+
 def base_linux_configuration():
     _apt_update()
-    put(os.path.join(__path__[0], "BaseLinux", "sshd_config"), "/etc/ssh", use_sudo=True)
+    put(os.path.join(base, "BaseLinux", "sshd_config"), "/etc/ssh", use_sudo=True)
     if not contains("/etc/environment", "RUNZ_RABBITMQ_SERVER"):
         append("/etc/environment", "RUNZ_RABBITMQ_SERVER=%s" % env.roledefs['queue'], use_sudo=True)
     if not contains("/etc/environment", "RUNZ_RIAK_HOST"):
@@ -30,19 +31,20 @@ def base_linux_configuration():
 
 
 def install_mono(version):
-    if not exists("/opt/mono-{mono_version}".format(mono_version = version)):
+    if not exists("/opt/mono-{mono_version}".format(mono_version=version)):
         sudo("apt-get -y install p7zip-full")
-        put(agent_folder+"/bins/mono-%s.7z" % version)
+        put(agent_folder + "/bins/mono-%s.7z" % version)
         run("rm -rdf mono-%s" % version)
         run("7z x mono-%s.7z" % version)
         sudo("rm -rdf /opt/mono-%s && mv mono-%s /opt" % (version, version))
         _generate_mono_wrapper_script(version)
 
+
 def build_mono(version, libgdi_version):
+    sudo(
+        "apt-get -y install build-essential autoconf automake bison libcairo2-dev libpango1.0-dev libfreetype6-dev libexif-dev libjpeg62-dev libtiff4-dev libgif-dev zlib1g-dev")
 
-    sudo("apt-get -y install build-essential autoconf automake bison libcairo2-dev libpango1.0-dev libfreetype6-dev libexif-dev libjpeg62-dev libtiff4-dev libgif-dev zlib1g-dev")
-
-    with process_erb(agent_folder +"/compile_script.sh.erb", {'MONO_VERSION' : version}) as f:
+    with process_erb(agent_folder + "/compile_script.sh.erb", {'MONO_VERSION': version}) as f:
         put(f.name, "compile_script.sh")
     run("chmod +x compile_script.sh && ./compile_script.sh")
     _generate_mono_wrapper_script(version)
@@ -62,25 +64,28 @@ def build_mono(version, libgdi_version):
 
 
 def _generate_mono_wrapper_script(version):
-    with process_erb(agent_folder+"/mono.erb", {'MONO_VERSION' : version}) as f:
-        put(f.name, "/usr/local/bin/mono-%s" % version, use_sudo = True)
+    with process_erb(agent_folder + "/mono.erb", {'MONO_VERSION': version}) as f:
+        put(f.name, "/usr/local/bin/mono-%s" % version, use_sudo=True)
     sudo("chmod +x /usr/local/bin/mono-%s" % version)
 
+
 def _install(packages):
-    sudo("apt-get -y install "+packages)
-    
+    sudo("apt-get -y install " + packages)
+
+
 def _run_background_process(command, output_prefix):
     # see http://stackoverflow.com/questions/29142/getting-ssh-to-execute-a-command-in-the-background-on-target-machine
-    run("nohup {command} > ~/{prefix}.out 2> ~/{prefix}.err < /dev/null".format(prefix = output_prefix, command = command))
-    
+    run("nohup {command} > ~/{prefix}.out 2> ~/{prefix}.err < /dev/null".format(prefix=output_prefix, command=command))
+
 
 def process_erb(file, kwargs):
-    for key,value in kwargs.iteritems():
-            os.environ[key] = value
+    for key, value in kwargs.iteritems():
+        os.environ[key] = value
 
     tempf = tempfile.NamedTemporaryFile()
     local("erb %s > %s" % (file, tempf.name))
     return tempf
+
 
 def _apt_update():
     sudo("apt-get -y update")
