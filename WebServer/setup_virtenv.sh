bbc84d9b0a3843b8254306ef1730a52ab11bf866
Petar Shomov <petar@spretturmarimo.is>
2011-05-30 23:21:00 +0000

    simplifying the web server deployment

--- WebServer/setup_virtenv.sh
+++ WebServer/setup_virtenv.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 echo "Preparing virtual env for app"
-sudo easy_install -U virtualenv || { echo "easy_install failed"; exit 1; } 
-virtualenv --distribute webz_virtenv || { echo "virtualenv failed"; exit 1; } 
-source ./webz_virtenv/bin/activate 
+virtualenv --distribute webz_virtenv || { echo "virtualenv failed"; exit 1; }
+source webz_virtenv/bin/activate
 pip install bobo || { echo "installing bobo failed"; exit 1; } 
 pip install gunicorn || { echo "installing gunicorn failed"; exit 1; } 
 pip install riak || { echo "installing riak python client failed"; exit 1; } 
