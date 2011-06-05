2257f195d69ead6dc48adac82f97f12e4ed8898c
Petar Shomov <petar@spretturmarimo.is>
2011-06-05 14:03:13 +0000

    manually fixing up the renaming of the packages, pycharm did not do all that great job on this one

--- spin_web_server.sh
+++ spin_web_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-fab spin_web_server reconfigure_server --user=web --password=web -i WebServer/web_id
+fab spin_web_server reconfigure_server --user=web --password=web -i web_server/web_id
