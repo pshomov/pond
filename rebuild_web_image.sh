00d6f9e5142a444498e9a9fced740e680c9f7385
Petar Shomov <petar@spretturmarimo.is>
2011-06-05 14:03:13 +0000

    manually fixing up the renaming of the packages, pycharm did not do all that great job on this one

--- rebuild_web_image.sh
+++ rebuild_web_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-fab cleanup create_web_server setup_web_server && fab select_last_server deploy_web:../frog/output store_web_image --user web -i WebServer/web_id
+fab cleanup create_web_server setup_web_server && fab select_last_server deploy_web:../frog/output store_web_image --user web -i web_server/web_id
