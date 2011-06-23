2b1f56825880ed1d849b482f98af078574d88bb7
Petar Shomov <petar@sprettur.is>
2011-06-23 00:00:56 +0000

    adding script for redeploying agents

---
+++ redeploy_agent_server.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from porcelan_support import launch_and_wait
+
+launch_and_wait("fab select_server:s-agent shutdown_agent_server deploy_agent:../frog/output/ start_agent_server --user=agent -i agent_server/agent_id")
