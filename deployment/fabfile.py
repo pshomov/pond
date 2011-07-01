36826a998a6aa5190e540502af618150283b7666
Petar Shomov <petar@sprettur.is>
2011-07-01 09:33:36 +0000

    just stop twitching and go down already, will you? there ... and stay down ;)

--- deployment/fabfile.py
+++ deployment/fabfile.py
@@ -15,7 +15,7 @@ def deploy_web(build_output):
     run("mkdir -p runz/webz")
     put(WEB_ARCHIVE, "runz/")
 
-    run("7z x -orunz runz/%s" % WEB_ARCHIVE)
+    run("7z x -y -orunz runz/%s" % WEB_ARCHIVE)
     run("chmod -R 755 runz/webz")
 
 def deploy_web_status(build_output):
@@ -29,7 +29,7 @@ def deploy_web_status(build_output):
     run("mkdir -p runz/web")
     put(WEB_ARCHIVE, "runz/")
 
-    run("7z x -orunz runz/%s" % WEB_ARCHIVE)
+    run("7z x -y -orunz runz/%s" % WEB_ARCHIVE)
     run("chmod -R 755 runz/web")
 
 
@@ -37,7 +37,7 @@ def start_web_server():
     run("~/runz-webz.sh start")
 
 def start_web_server_status():
-    run("~/runz-web.sh start")
+    fabutils._run_background_process("~/runz-web.sh start", "web")
 
 
 def shutdown_web_server():
