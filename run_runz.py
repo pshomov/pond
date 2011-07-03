ad5e2313d817f67480185ef06343e10fe6db467e
Petar Shomov <petar@sprettur.is>
2011-07-03 22:27:38 +0000

    updating the script to launch a complete Runz system to use the 'spin' scripts instead of the 'new' scripts since they are much faster

--- run_runz.py
+++ run_runz.py
@@ -2,12 +2,12 @@
 from porcelan_support import launch
 
 
-p1 = launch("./new_store_server.py")
-p2 = launch("./new_queue_server.py")
+p1 = launch("./spin_store_server.py")
+p2 = launch("./spin_queue_server.py")
 
 if p1.wait() == 0 and p2.wait() == 0:
-    p3 = launch("./new_web_server.py")
-    p4 = launch("./new_agent_server.py")
-    p5 = launch("./new_repotracker_server.py")
+    p3 = launch("./spin_web_server.py")
+    p4 = launch("./spin_agent_server.py")
+    p5 = launch("./spin_repotracker_server.py")
     exit(p3.wait() or p4.wait() or p5.wait())
 exit(2)
