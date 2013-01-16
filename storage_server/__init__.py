e03a228b528af67b64bca842b4a5e2c5575587a1
Petar Shomov <pshomov@gmail.com>
2013-01-16 00:50:46 +0000

    deploying a newer riak server

--- storage_server/__init__.py
+++ storage_server/__init__.py
@@ -4,12 +4,12 @@ from fabric.operations import sudo
 from fabric.state import env
 import fabutils
 
-RIAK_VERSION = "1.0.1-1"
+RIAK_VERSION = "1.2.1-1"
 RIAK_ARCH = "amd64"
 
 def prepare():
     """ prepares the server for installing riak"""
-    sudo("apt-get -y install wget")
+    sudo("apt-get -y install wget libssl0.9.8 curl")
 
 def accounts():
     base_folder = os.path.normpath(os.path.abspath(os.path.dirname(__file__)))
@@ -18,13 +18,15 @@ def accounts():
 def install_riak():
     """ installs riak storage server """
     riak_deb = "riak_%s_%s.deb" % (RIAK_VERSION, RIAK_ARCH)
-    riak_url = "http://downloads.basho.com/riak/riak-1.0.1/%s" % riak_deb
+    riak_url = "http://s3.amazonaws.com/downloads.basho.com/riak/1.2/1.2.1/ubuntu/natty/riak_1.2.1-1_amd64.deb"
     run("wget %s" % riak_url)
     sudo("dpkg -i %s" % riak_deb)
-    # TODO: set name of the node and IP address in config files prior to upload
+
     put(os.path.join(__path__[0], "conf", "app.config"), "/etc/riak/", use_sudo=True)
     put(os.path.join(__path__[0], "conf", "vm.args"), "/etc/riak", use_sudo=True)
-    
+    put(os.path.join(__path__[0], "conf", "cert.pem"), "/etc/riak", use_sudo=True)
+    put(os.path.join(__path__[0], "conf", "key.pem"), "/etc/riak", use_sudo=True)
+
     sudo("update-rc.d riak defaults")
     start_riak()
     sudo("curl -s rekon.basho.com | sh")
